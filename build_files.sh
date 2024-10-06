9b679faf3c749c4906879107e8d47a5572e3c136
NguyenSongHau <nguyensonghau27072003@gmail.com>
2024-10-07 06:14:09 +0700

    delete file

--- build_files.sh
+++
@@ -1,5 +0,0 @@
-pip3 install -r requirements.txt
-python3.12 manage.py collectstatic --noinput
-python3.12 manage.py makemigrations --noinput
-python3.12 manage.py migrate --noinput
-python3.12 manage.py collectdata
