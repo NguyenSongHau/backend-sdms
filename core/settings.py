16c946530001360e44b604f9969132b3df078a8b
NguyenSongHau <nguyensonghau27072003@gmail.com>
2024-10-07 07:03:58 +0700

    fix vercel

--- core/settings.py
+++ core/settings.py
@@ -133,9 +133,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 STATIC_URL = "/static/"
 
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles_build", "static")
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles_build")
 
-STATICFILES_DIRS = [os.path.join(BASE_DIR, "static"), ]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "staticfiles_build", "static")]
+
 
 MEDIA_URL = "/media/"
 
