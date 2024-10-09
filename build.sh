1d93a8218738cd3963d341dccbd2d1c2bdb02c9f
NguyenSongHau <nguyensonghau27072003@gmail.com>
2024-10-09 23:22:51 +0700

    fix vercel

--- build.sh
+++ build.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-pip install -r requirment.txt
+python3.12 -m pip install -r requirment.txt
 
-python manage.py makemigrations --noinput
+python3.12 manage.py makemigrations --noinput
 
-python manage.py migrate
+python3.12 manage.py migrate
 
-python manage.py collectstatic --noinput --clear
+python3.12 manage.py collectstatic --noinput --clear
