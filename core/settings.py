9d2cccd398db4b8b6bab89907cfb578bcf857d71
NguyenSongHau <nguyensonghau27072003@gmail.com>
2024-10-09 16:31:51 +0700

    add database

--- core/settings.py
+++ core/settings.py
@@ -103,7 +103,8 @@ DATABASES = {
         'NAME': os.getenv('DATABASE'),
         'USER': os.getenv('USER'),
         'PASSWORD': os.getenv('PASSWORD'),
-        'HOST': os.getenv('HOST')
+        'HOST': os.getenv('HOST'),
+        'PORT': os.getenv('PORT')
     }
 }
 
