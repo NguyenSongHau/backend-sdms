81fc25b5b180156a5269583667c9f83b1cb027d6
NguyenSongHau <nguyensonghau27072003@gmail.com>
2024-10-17 13:26:20 +0700

    Fix API

--- rental/serializers.py
+++ rental/serializers.py
@@ -7,211 +7,198 @@ from users import serializers as user_serializers
 
 
 class RoomSerializer(BaseSerializer):
-    beds = serializers.SerializerMethodField()
+	beds = serializers.SerializerMethodField()
 
-    class Meta:
-        model = Room
-        fields = ["id", "name", "image", "number_of_bed", "type", "room_for", "created_date", "updated_date", "beds"]
+	class Meta:
+		model = Room
+		fields = ["id", "name", "image", "number_of_bed", "type", "room_for", "created_date", "updated_date", "beds"]
 
-    def to_representation(self, room):
-        data = super().to_representation(room)
-        image = data.get("image")
+	def to_representation(self, room):
+		data = super().to_representation(room)
+		image = data.get("image")
 
-        if "image" in self.fields and image:
-            data["image"] = room.image.url
+		if "image" in self.fields and image:
+			data["image"] = room.image.url
 
-        return data
+		return data
 
-    def update(self, room, validated_data):
-        validated_data["number_of_bed"] = None
+	def update(self, room, validated_data):
+		validated_data["number_of_bed"] = None
 
-        for key, value in validated_data.items():
-            setattr(room, key, value)
-        room.save()
+		for key, value in validated_data.items():
+			setattr(room, key, value)
+		room.save()
 
-        return room
+		return room
 
-    def get_beds(self, room):
-        beds = room.beds.all()
+	def get_beds(self, room):
+		beds = room.beds.all()
 
-        return BedSerializer(beds, many=True).data
+		return BedSerializer(beds, many=True).data
 
 
 class PostSerializer(BaseSerializer):
-    total_likes = serializers.SerializerMethodField()
+	total_likes = serializers.SerializerMethodField()
 
-    class Meta:
-        model = Post
-        fields = ["id", "name", "image", "description", "created_date", "updated_date", "total_likes", "room"]
+	class Meta:
+		model = Post
+		fields = ["id", "name", "image", "description", "created_date", "updated_date", "total_likes", "room"]
 
-    def to_representation(self, post):
-        data = super().to_representation(post)
-        image = data.get("image")
+	def to_representation(self, post):
+		data = super().to_representation(post)
+		image = data.get("image")
 
-        if self.context.get('action') == 'list':
-            data.pop('room', None)
-        else:
-            if "room" in self.fields and post.room:
-                data["room"] = RoomSerializer(post.room).data
-                if hasattr(post.room, 'bed'):
-                    data["bed"] = BedSerializer(post.room.bed).data
+		if self.context.get('action') == 'list':
+			data.pop('room', None)
+		else:
+			if "room" in self.fields and post.room:
+				data["room"] = RoomSerializer(post.room).data
+				if hasattr(post.room, 'bed'):
+					data["bed"] = BedSerializer(post.room.bed).data
 
-        if "image" in self.fields and image:
-            data["image"] = post.image.url
+		if "image" in self.fields and image:
+			data["image"] = post.image.url
 
-        return data
+		return data
 
-    def update(self, post, validated_data):
-        for key, value in validated_data.items():
-            setattr(post, key, value)
-        post.save()
+	def update(self, post, validated_data):
+		for key, value in validated_data.items():
+			setattr(post, key, value)
+		post.save()
 
-        return post
+		return post
 
-    def get_total_likes(self, post):
-        return post.likes.filter(is_active=True).count()
+	def get_total_likes(self, post):
+		return post.likes.filter(is_active=True).count()
 
 
 class AuthenticatedPostSerializer(PostSerializer):
-    liked = serializers.SerializerMethodField()
+	liked = serializers.SerializerMethodField()
 
-    class Meta:
-        model = PostSerializer.Meta.model
-        fields = PostSerializer.Meta.fields + ["liked"]
+	class Meta:
+		model = PostSerializer.Meta.model
+		fields = PostSerializer.Meta.fields + ["liked"]
 
-    def get_liked(self, post):
-        request = self.context.get("request")
+	def get_liked(self, post):
+		request = self.context.get("request")
 
-        try:
-            like = Like.objects.get(user=request.user, post=post)
-        except Like.DoesNotExist:
-            return False
+		try:
+			like = Like.objects.get(user=request.user, post=post)
+		except Like.DoesNotExist:
+			return False
 
-        return like.is_active
+		return like.is_active
 
 
 class BedSerializer(BaseSerializer):
-    class Meta:
-        model = Bed
-        fields = ["id", "name", "price", "image", "description", "status", "created_date", "updated_date", "room"]
-        extra_kwargs = {"room": {"write_only": True}}
+	class Meta:
+		model = Bed
+		fields = ["id", "name", "price", "image", "description", "status", "created_date", "updated_date", "room"]
+		extra_kwargs = {"room": {"write_only": True}}
 
-    def to_representation(self, bed):
-        data = super().to_representation(bed)
-        image = data.get("image")
+	def to_representation(self, bed):
+		data = super().to_representation(bed)
+		image = data.get("image")
 
-        if "image" in self.fields and image:
-            data["image"] = bed.image.url
+		if "image" in self.fields and image:
+			data["image"] = bed.image.url
 
-        return data
+		return data
 
-    def update(self, bed, validated_data):
-        validated_data["price"] = None
+	def update(self, bed, validated_data):
+		validated_data["price"] = None
 
-        for key, value in validated_data.items():
-            setattr(bed, key, value)
-        bed.save()
+		for key, value in validated_data.items():
+			setattr(bed, key, value)
+		bed.save()
 
-        return bed
+		return bed
 
-    def validate(self, data):
-        room = data.get('room')
-        if room:
-            existing_beds_count = room.beds.count()
-            number_of_beds = room.number_of_bed
+	def validate(self, data):
+		room = data.get('room')
+		if room:
+			existing_beds_count = room.beds.count()
+			number_of_beds = room.number_of_bed
 
-            if existing_beds_count >= number_of_beds:
-                raise serializers.ValidationError({"message": f"{room.name} đã đủ {number_of_beds} giường."})
+			if existing_beds_count >= number_of_beds:
+				raise serializers.ValidationError({"message": f"{room.name} đã đủ {number_of_beds} giường."})
 
-        return data
+		return data
 
 
 class RentalContactSerializer(BaseSerializer):
     class Meta:
         model = RentalContact
-        fields = ["id", "rental_number", "time_rental", "status", "created_date", "updated_date", "bed", "student",
-                  "room"]
+        fields = ["id", "rental_number", "time_rental", "status", "created_date", "updated_date", "bed", "student", "room"]
 
-        def to_representation(self, rental_contact):
-            data = super().to_representation(rental_contact)
-            student = data.get("student")
-            bed = data.get("bed")
-            room = data.get("room")
+    def to_representation(self, rental_contact):
+        data = super().to_representation(rental_contact)
+        student = data.get("student")
+        bed = data.get("bed")
+        room = data.get("room")
 
-            if "student" in self.fields and student:
-                data["student"] = user_serializers.StudentSerializer(rental_contact.student).data
-            if "bed" in self.fields and bed:
-                data["bed"] = BedSerializer(rental_contact.bed).data
+        if "student" in self.fields and student:
+            data["student"] = user_serializers.StudentSerializer(rental_contact.student).data
+        if "bed" in self.fields and bed:
+            data["bed"] = BedSerializer(rental_contact.bed).data
+        if "room" in self.fields and room:
+            data["room"] = RoomSerializer(rental_contact.room).data
 
-            if "room" in self.fields and room:
-                data["room"] = {
-                    "id": rental_contact.bed.room.id,
-                    "name": rental_contact.bed.room.name,
-                    "image": rental_contact.bed.room.image,
-                    "number_of_bed": rental_contact.bed.room.number_of_bed,
-                    "type": rental_contact.bed.room.type,
-                    "room_for": rental_contact.bed.room.room_for,
-                    "created_date": rental_contact.bed.room.created_date,
-                    "updated_date": rental_contact.bed.room.updated_date,
-                }
-
-            return data
+        return data
 
 
 class BillRentalContactSerializer(BaseSerializer):
-    class Meta:
-        model = BillRentalContact
-        fields = ["id", "bill_number", "total", "status", "created_date", "updated_date", "student", "specialist",
-                  "rental_contact"]
+	class Meta:
+		model = BillRentalContact
+		fields = ["id", "bill_number", "total", "status", "created_date", "updated_date", "student", "specialist", "rental_contact"]
 
-    def to_representation(self, bill_rental_contact):
-        data = super().to_representation(bill_rental_contact)
-        student = data.get("student")
-        specialist = data.get("specialist")
-        rental_contact = data.get("rental_contact")
+	def to_representation(self, bill_rental_contact):
+		data = super().to_representation(bill_rental_contact)
+		student = data.get("student")
+		specialist = data.get("specialist")
+		rental_contact = data.get("rental_contact")
 
-        if "student" in self.fields and student:
-            data["student"] = user_serializers.StudentSerializer(bill_rental_contact.student).data
-        if "specialist" in self.fields and specialist:
-            data["specialist"] = user_serializers.UserSerializer(bill_rental_contact.specialist.user).data
-        if "rental_contact" in self.fields and rental_contact:
-            data["rental_contact"] = RentalContactSerializer(bill_rental_contact.rental_contact).data
+		if "student" in self.fields and student:
+			data["student"] = user_serializers.StudentSerializer(bill_rental_contact.student).data
+		if "specialist" in self.fields and specialist:
+			data["specialist"] = user_serializers.UserSerializer(bill_rental_contact.specialist.user).data
+		if "rental_contact" in self.fields and rental_contact:
+			data["rental_contact"] = RentalContactSerializer(bill_rental_contact.rental_contact).data
 
-        return data
+		return data
 
 
 class ViolateNoticeSerializer(BaseSerializer):
-    class Meta:
-        model = ViolateNotice
-        fields = ["id", "violate_number", "description", "created_date", "updated_date", "manager", "room"]
+	class Meta:
+		model = ViolateNotice
+		fields = ["id", "violate_number", "description", "created_date", "updated_date", "manager", "room"]
 
-    def to_representation(self, violate_notice):
-        data = super().to_representation(violate_notice)
-        manager = data.get("manager")
-        room = data.get("room")
+	def to_representation(self, violate_notice):
+		data = super().to_representation(violate_notice)
+		manager = data.get("manager")
+		room = data.get("room")
 
-        if "manager" in self.fields and manager:
-            data["manager"] = user_serializers.UserSerializer(violate_notice.manager.user).data
-        if "room" in self.fields and room:
-            data["room"] = RoomSerializer(violate_notice.room, excludes=["beds"]).data
+		if "manager" in self.fields and manager:
+			data["manager"] = user_serializers.UserSerializer(violate_notice.manager.user).data
+		if "room" in self.fields and room:
+			data["room"] = RoomSerializer(violate_notice.room, excludes=["beds"]).data
 
-        return data
+		return data
 
 
 class ElectricityAndWaterBillsSerializer(BaseSerializer):
-    class Meta:
-        model = ElectricityAndWaterBills
-        fields = ["id", "total_cubic_meters_water", "total_electricity", "total_amount", "status", "created_date",
-                  "updated_date", "manager", "room"]
+	class Meta:
+		model = ElectricityAndWaterBills
+		fields = ["id", "total_cubic_meters_water", "total_electricity", "total_amount", "status", "created_date", "updated_date", "manager", "room"]
 
-    def to_representation(self, electricity_and_water_bills):
-        data = super().to_representation(electricity_and_water_bills)
-        manager = data.get("manager")
-        room = data.get("room")
+	def to_representation(self, electricity_and_water_bills):
+		data = super().to_representation(electricity_and_water_bills)
+		manager = data.get("manager")
+		room = data.get("room")
 
-        if "manager" in self.fields and manager:
-            data["manager"] = user_serializers.UserSerializer(electricity_and_water_bills.manager.user).data
-        if "room" in self.fields and room:
-            data["room"] = RoomSerializer(electricity_and_water_bills.room, excludes=["beds"]).data
+		if "manager" in self.fields and manager:
+			data["manager"] = user_serializers.UserSerializer(electricity_and_water_bills.manager.user).data
+		if "room" in self.fields and room:
+			data["room"] = RoomSerializer(electricity_and_water_bills.room, excludes=["beds"]).data
 
-        return data
+		return data
