018f0fe65a781a6fb8d5632d7481d9cfe3e8ecc0
NguyenSongHau <nguyensonghau27072003@gmail.com>
2024-11-19 09:55:07 +0700

    Add Filter Rental Contact For Student

--- users/views.py
+++ users/views.py
@@ -14,93 +14,112 @@ from users.serializers import UserSerializer
 
 
 class UserViewSet(viewsets.ViewSet):
-	queryset = User.objects.filter(is_active=True)
-	serializer_class = users_serializers.UserSerializer
-	parser_classes = [parsers.MultiPartParser]
+    queryset = User.objects.filter(is_active=True)
+    serializer_class = users_serializers.UserSerializer
+    parser_classes = [parsers.MultiPartParser]
 
-	def get_queryset(self):
-		queryset = self.queryset
+    def get_queryset(self):
+        queryset = self.queryset
 
-		queryset = queryset.prefetch_related("rental_contacts") if self.action.__eq__("get_all_rental_contacts") else queryset
+        queryset = queryset.prefetch_related("rental_contacts") if self.action.__eq__(
+            "get_all_rental_contacts") else queryset
 
-		return queryset
+        return queryset
 
-	def get_permissions(self):
-		if self.action in ["current_user", "update_current_user"]:
-			return [permissions.IsAuthenticated()]
+    def get_permissions(self):
+        if self.action in ["current_user", "update_current_user"]:
+            return [permissions.IsAuthenticated()]
 
-		if self.action in ["get_all_rental_contacts", "get_rental_contact_detail"]:
-			return [perms.IsStudent()]
+        if self.action in ["get_all_rental_contacts", "get_rental_contact_detail"]:
+            return [perms.IsStudent()]
 
-		return [permissions.AllowAny()]
+        return [permissions.AllowAny()]
 
-	@action(methods=["post"], detail=False, url_path="register")
-	def register(self, request):
-		serializer = self.serializer_class(data=request.data)
-		serializer.is_valid(raise_exception=True)
-		serializer.save()
+    @action(methods=["post"], detail=False, url_path="register")
+    def register(self, request):
+        serializer = self.serializer_class(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
 
-		return Response(data={"message": "Đăng ký tài khoản thành công."}, status=status.HTTP_201_CREATED)
+        return Response(data={"message": "Đăng ký tài khoản thành công."}, status=status.HTTP_201_CREATED)
 
-	@action(methods=["get"], detail=False, url_path="current-user")
-	def current_user(self, request):
-		serializer = self.serializer_class(request.user)
+    @action(methods=["get"], detail=False, url_path="current-user")
+    def current_user(self, request):
+        serializer = self.serializer_class(request.user)
 
-		return Response(data=serializer.data, status=status.HTTP_200_OK)
+        return Response(data=serializer.data, status=status.HTTP_200_OK)
 
-	@action(methods=["patch"], detail=False, url_path="current-user/update")
-	def update_current_user(self, request):
-		old_password = request.data.get("old_password", None)
-		new_password = request.data.get("new_password", None)
+    @action(methods=["patch"], detail=False, url_path="current-user/update")
+    def update_current_user(self, request):
+        old_password = request.data.get("old_password", None)
+        new_password = request.data.get("new_password", None)
 
-		if old_password and new_password and not check_password(old_password, request.user.password):
-			return Response(data={"detail": "Mật khẩu cũ không chính xác"}, status=status.HTTP_400_BAD_REQUEST)
+        if old_password and new_password and not check_password(old_password, request.user.password):
+            return Response(data={"detail": "Mật khẩu cũ không chính xác"}, status=status.HTTP_400_BAD_REQUEST)
 
-		serializer = users_serializers.UserUpdateSerializer(instance=request.user, data=request.data, partial=True)
-		serializer.is_valid(raise_exception=True)
-		serializer.save()
+        serializer = users_serializers.UserUpdateSerializer(instance=request.user, data=request.data, partial=True)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
 
-		response_data = serializer.data
-		if request.user.dob:
-			response_data['dob'] = request.user.dob.strftime("%d-%m-%Y")
+        response_data = serializer.data
+        if request.user.dob:
+            response_data['dob'] = request.user.dob.strftime("%d-%m-%Y")
 
-		return Response(data=response_data, status=status.HTTP_200_OK)
+        return Response(data=response_data, status=status.HTTP_200_OK)
 
-	@action(methods=["get"], detail=False, url_path="students/rental-contacts")
-	def get_all_rental_contacts(self, request):
-		student = request.user.student
-		rental_status = request.query_params.get("status")
-		rental_contacts = student.rental_contacts.filter(status=rental_status.upper()) if rental_status else student.rental_contacts.all()
+    @action(methods=["get"], detail=False, url_path="students/rental-contacts")
+    def get_all_rental_contacts(self, request):
+        student = request.user.student
+        rental_contacts = student.rental_contacts.all()
 
-		paginator = paginators.RentalContactPaginators()
-		page = paginator.paginate_queryset(queryset=rental_contacts, request=request)
-		if page is not None:
-			serializer = rental_serializers.RentalContactSerializer(page, many=True)
-			return paginator.get_paginated_response(serializer.data)
+        rental_number = request.query_params.get("rental_number")
+        if rental_number:
+            rental_contacts = rental_contacts.filter(rental_number=rental_number)
 
-		serializer = rental_serializers.RentalContactSerializer(rental_contacts, many=True)
-		return Response(data=serializer.data, status=status.HTTP_200_OK)
+        rental_status = request.query_params.get("status")
+        if rental_status:
+            rental_contacts = rental_contacts.filter(status=rental_status.upper())
 
-	@action(methods=["get"], detail=False, url_path="students/rental-contacts/(?P<pk>[^/.]+)")
-	def get_rental_contact_detail(self, request, pk=None):
-		rental_contact = get_object_or_404(RentalContact, id=pk)
-		serializer = rental_serializers.RentalContactSerializer(rental_contact)
-		return Response(data=serializer.data, status=status.HTTP_200_OK)
+        student_id = request.query_params.get("student_id")
+        if student_id:
+            rental_contacts = rental_contacts.filter(student_id=student_id)
 
-	@action(methods=["get"], detail=False, url_path="specialists-managers")
-	def get_all_specialists_and_managers(self, request):
-		specialists = Specialist.objects.filter(user__is_active=True)
-		managers = Manager.objects.filter(user__is_active=True)
+        bed_id = request.query_params.get("bed_id")
+        if bed_id:
+            rental_contacts = rental_contacts.filter(bed_id=bed_id)
 
-		specialist_users = User.objects.filter(specialist__in=specialists)
-		manager_users = User.objects.filter(manager__in=managers)
+        room_id = request.query_params.get("room_id")
+        if room_id:
+            rental_contacts = rental_contacts.filter(bed__room_id=room_id)
 
-		combined_users = specialist_users | manager_users
+        paginator = paginators.RentalContactPaginators()
+        page = paginator.paginate_queryset(queryset=rental_contacts, request=request)
+        if page is not None:
+            serializer = rental_serializers.RentalContactSerializer(page, many=True)
+            return paginator.get_paginated_response(serializer.data)
 
-		paginator = UserPagination()
-		paginated_users = paginator.paginate_queryset(combined_users, request)
+        serializer = rental_serializers.RentalContactSerializer(rental_contacts, many=True)
+        return Response(data=serializer.data, status=status.HTTP_200_OK)
 
-		serializer = UserSerializer(paginated_users, many=True)
+    @action(methods=["get"], detail=False, url_path="students/rental-contacts/(?P<pk>[^/.]+)")
+    def get_rental_contact_detail(self, request, pk=None):
+        rental_contact = get_object_or_404(RentalContact, id=pk)
+        serializer = rental_serializers.RentalContactSerializer(rental_contact)
+        return Response(data=serializer.data, status=status.HTTP_200_OK)
 
-		return paginator.get_paginated_response(serializer.data)
+    @action(methods=["get"], detail=False, url_path="specialists-managers")
+    def get_all_specialists_and_managers(self, request):
+        specialists = Specialist.objects.filter(user__is_active=True)
+        managers = Manager.objects.filter(user__is_active=True)
 
+        specialist_users = User.objects.filter(specialist__in=specialists)
+        manager_users = User.objects.filter(manager__in=managers)
+
+        combined_users = specialist_users | manager_users
+
+        paginator = UserPagination()
+        paginated_users = paginator.paginate_queryset(combined_users, request)
+
+        serializer = UserSerializer(paginated_users, many=True)
+
+        return paginator.get_paginated_response(serializer.data)
